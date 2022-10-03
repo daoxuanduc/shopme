342af7a65583a84b9940d47d5ab05410d22e07c9
daoxuanduc <daoxuanduc3006@gmail.com>
2022-10-03 22:34:07 +0700

    take note

--- ShopmeWebParent/ShopmeAdmin/src/main/java/com/shopme/admin/ShopmeAdminApplication.java
+++ ShopmeWebParent/ShopmeAdmin/src/main/java/com/shopme/admin/ShopmeAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"})
+@EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"}) //Error: Not a managed type: class com.shopme.common.entity.Role
 public class ShopmeAdminApplication {
 
 	public static void main(String[] args) {
--- ShopmeWebParent/ShopmeAdmin/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
+++ ShopmeWebParent/ShopmeAdmin/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -13,17 +13,19 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@DataJpaTest  //
+@AutoConfigureTestDatabase(replace = Replace.NONE) //for testing real DB, not in-memory DB
 @Rollback(false)
 public class RoleRepositoryTests {
-	@Autowired
-	private RoleRepository repo;
+	@Autowired //let Spring framework inject an instance of RoleRepository interface
+	//sau khi tạo interface RoleRepository, Spring sẽ quét các class cùng cấp hoặc thấp hơn để tìm các
+	//instance đánh dấu @Component (bao gồm cả @Repository phục vụ truy xuất dữ liệu). @Autowired ở đây để lấy được các instance này
+	private RoleRepository roleRepository;
 	
 	@Test
 	public void testCreateFirstRole() {
 		Role roleAdmin = new Role("Admin", "manage everything");
-		Role saveRole = repo.save(roleAdmin);
+		Role saveRole = roleRepository.save(roleAdmin);
 		
 		assertThat(saveRole.getId()).isGreaterThan(0);
 	}
@@ -40,7 +42,7 @@ public class RoleRepositoryTests {
 				+ "and update order status");
 		
 		Role roleAssistant = new Role("Assistant", "manage questions and reviews");
-		
-		repo.saveAll(List.of(roleSalesperson, roleEditor, roleShiper, roleAssistant));
+
+		roleRepository.saveAll(List.of(roleSalesperson, roleEditor, roleShiper, roleAssistant));
 	}
 }
